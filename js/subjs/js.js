0f435d011371cb315a7b6fb576788de7bf6cf4f8
Emanuel LH <ejlhidalgo@gmail.com>
2026-08-14 21:28:36 -0600

    Aplicar todos los cambios del proyecto

---
+++ js/subjs/js.js
@@ -0,0 +1,10 @@
+const appState = {
+  currentView: 'lista', // 'lista', 'registro', 'detalle', 'prorroga'
+  currentId: null,
+  filtro: {
+    busqueda: '',
+    estado: 'Todas',
+    departamento: 'Todos'
+  },
+  modo: 'crear' // 'crear' o 'editar'
+};
